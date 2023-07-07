3f2342b93e91e96fbfbb0240fb6a364f768538a8
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-07-06 19:55:31 -0700

    inital commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+PKGNAME := alias
+STRIP := llvm-strip
+CC := clang
+
+ifdef VERBOSE
+	SHELL += -x
+endif
+
+OPTFLAGS += -Os -fno-asynchronous-unwind-tables
+CFLAGS += -Wall -Wextra -Werror -Wno-unused-command-line-argument -std=c99
+LDFLAGS += -fuse-ld=lld -nostdlib -lkernel32
+
+all: $(PKGNAME).c
+	@printf " \033[1;32mCC\033[0m %s\n" "$(PKGNAME).c"
+	@$(CC) $(CFLAGS) $(LDFLAGS) $(OPTFLAGS) -o $(PKGNAME).exe $(PKGNAME).c -s
+	@$(STRIP) $(PKGNAME).exe
+
+debug:
+	@printf " \033[1;33mCC\033[0m %s\n" "$(PKGNAME).c"
+	@$(CC) $(CFLAGS) $(LDFLAGS) $(OPTFLAGS) -o $(PKGNAME).exe $(PKGNAME).c -g
+
+clean:
+	@printf "Cleaning...\n"
+	@rm -f $(PKGNAME).exe
---
+++ alias.c
@@ -0,0 +1,25 @@
+// Well-behaved command line aliases
+// Unlike batch script aliases, this program will not produce an annoying and useless "Terminate batch job (Y/N)" prompt.
+
+#include <windows.h>
+#include <stdio.h>
+
+#define EXE L"C:\\Users\\Joey\\AppData\\Local\\BusyBox\\busybox.exe"
+#define CMD L"ash -l"
+
+_Static_assert(sizeof(EXE) <= MAX_PATH, "EXE length exceeds MAX_PATH");
+
+int mainCRTStartup(void) {
+    STARTUPINFOW si = {0};
+    si.cb = sizeof(si);
+    PROCESS_INFORMATION pi;
+
+    if (!CreateProcessW(EXE, CMD, 0, 0, 1, 0, 0, 0, &si, &pi)) {
+        return 1;
+    }
+
+    unsigned long ret;
+    WaitForSingleObject(pi.hProcess, -1);
+    GetExitCodeProcess(pi.hProcess, &ret);
+    return ret;
+}
